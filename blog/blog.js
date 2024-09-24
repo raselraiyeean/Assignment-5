07ba40bf15a1d679b9efbb6014f98720b2222431
Your Name <you@example.com>
2024-09-24 08:52:12 +0600

    blog page and its js added

---
+++ blog/blog.js
@@ -0,0 +1,3 @@
+document.getElementById('home-btn').addEventListener('click', function() {
+    window.location.href = "../home/index.html";
+})
--- home/donation-page.js
+++ home/donation-page.js
@@ -2,12 +2,16 @@
 document.getElementById('donate-history-button').addEventListener('click', function() {
     document.getElementById('history-page').classList.remove('hidden');
     document.getElementById('donate-page').classList.add('hidden');
+    document.getElementById('donate-money-button').classList.remove('bg-green-400')
+    document.getElementById('donate-history-button').classList.add('bg-green-400')
     
 });
 
     document.getElementById('donate-money-button').addEventListener('click', function() {
     document.getElementById('history-page').classList.add('hidden');
     document.getElementById('donate-page').classList.remove('hidden');
+    document.getElementById('donate-money-button').classList.add('bg-green-400')
+    document.getElementById('donate-history-button').classList.remove('bg-green-400')
     
 });
 
@@ -18,8 +22,8 @@ document.getElementById('noakhali-donate-button').addEventListener('click', func
     const mainBalanceDonationPage = textValueById('main-balance-of-donation-page');
     const donationBalanceForNoakhali = textValueById('donation-balance-for-Noakhali');
     if((donationAmountForNoakhali > mainBalanceDonationPage) || 
-        (donationAmountForNoakhali <= 0) || (typeof(donationAmountForNoakhali) !== 'number') 
-        || (donationAmountForNoakhali === "" )) {
+        (donationAmountForNoakhali <= 0) || isNaN(donationAmountForNoakhali)  
+        ) {
             alert('Invalid Input!! Give a valid Input.');
         }
         else {
@@ -27,7 +31,7 @@ document.getElementById('noakhali-donate-button').addEventListener('click', func
             const newBalanceForNoakhaliDonationFund = donationBalanceForNoakhali + donationAmountForNoakhali;
             document.getElementById('main-balance-of-donation-page').innerText = newMainBalance;
             document.getElementById('donation-balance-for-Noakhali').innerText = newBalanceForNoakhaliDonationFund;
-            alert('Congratulation! you donated for Noakhali flood affected people.'); 
+             
             const historyContainer = document.getElementById('history-page');
             const historyText = document.createElement('p');
             let currentDate = new Date();
@@ -50,8 +54,8 @@ document.getElementById('donate-btn-for-feni').addEventListener('click', functio
     const mainBalanceDonationPage = textValueById('main-balance-of-donation-page');
     const donationBalanceForFeni = textValueById('donation-balance-for-feni');
     if((donationAmountForFeni > mainBalanceDonationPage) || 
-    (donationAmountForFeni <= 0) || (typeof(donationAmountForFeni) !== 'number') 
-    || (donationAmountForFeni === '')) {
+    (donationAmountForFeni <= 0) || isNaN(donationAmountForFeni) 
+    ) {
         alert('Invalid Input!! Give a valid Input.');
     }
 
@@ -60,7 +64,7 @@ document.getElementById('donate-btn-for-feni').addEventListener('click', functio
         const newBalanceForFeniDonationFund = donationBalanceForFeni + donationAmountForFeni;
         document.getElementById('main-balance-of-donation-page').innerText = newMainBalance;
         document.getElementById('donation-balance-for-feni').innerText = newBalanceForFeniDonationFund;
-        alert('Congratulation! you donated for Noakhali flood affected people.');
+        
         const historyContainer = document.getElementById('history-page');
             const historyText = document.createElement('p');
             let currentDate = new Date();
@@ -80,8 +84,8 @@ document.getElementById('aid-btn-for-quota-movement-injured').addEventListener('
     const mainBalanceDonationPage = textValueById('main-balance-of-donation-page');
     const donationBalanceForInjuredPeople = textValueById('main-balance-for-injured-people');
     if((donationAmountForInjuredPeople > mainBalanceDonationPage) || 
-    (donationAmountForInjuredPeople <= 0) || (typeof(donationAmountForInjuredPeople) !== 'number') 
-    || (donationAmountForInjuredPeople === '')) {
+    (donationAmountForInjuredPeople <= 0) || isNaN(donationAmountForInjuredPeople)
+    ) {
         alert('Invalid Input!! Give a valid Input.');
     }
 
@@ -90,16 +94,26 @@ document.getElementById('aid-btn-for-quota-movement-injured').addEventListener('
         const newBalanceForInjuredPeople = donationBalanceForInjuredPeople + donationAmountForInjuredPeople;
         document.getElementById('main-balance-of-donation-page').innerText = newMainBalance;
         document.getElementById('main-balance-for-injured-people').innerText = newBalanceForInjuredPeople;
-        alert('Congratulation! you donated for Noakhali flood affected people.');
+        
         const historyContainer = document.getElementById('history-page');
             const historyText = document.createElement('p');
             let currentDate = new Date();
             
             historyText.innerHTML = donationAmountForInjuredPeople + 
-            ' Taka is donated for Flood at Feni, Bangladesh.' + '<br>' + currentDate;
+            ' Taka is donated for injured people in 2024 freedom fight, Bangladesh.' + '<br>' + currentDate;
             historyText.setAttribute("class", "card-normal");
             historyText.classList.add('card-body', 'border-2', 'my-5', 'font-semibold', 'text-2xl');
             historyContainer.appendChild(historyText);
     }  
 
 })
+
+// js to go blog page and return to home page
+document.getElementById('blog-btn').addEventListener('click', function() {
+    window.location.href = "../blog/index.html";
+})
+
+
+
+
+
